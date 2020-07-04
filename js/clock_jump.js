97ad036ef7f75c6106b09e4653428d3498f8fd75
xiangxiangxiong9 <1296976552@qq.com>
2020-07-04 21:11:40 +0800

    clock重新排序

--- js/clock_jump.js
+++ js/clock_jump.js
@@ -1,19 +1,19 @@
-function clock1(To){
-    i = i - 1;
-    document.title = "页面将在" + i + "内跳转";
-    if (i > 0) {
-        setTimeout("clock1('" + To + "')", 1000);
-    } else {
-    window.open(To + '.html');
-    window.opener = "test";
-    window.close();
-    window.opener = null;
-    window.open('', '_0');
-    window.close();
-    window.open("about:blank", "_0").close();
-    }
-}
 function clock(To, time){
+    function clock1(To){
+        i = i - 1;
+        document.title = "页面将在" + i + "内跳转";
+        if (i > 0) {
+            setTimeout("clock1('" + To + "')", 1000);
+        } else {
+        window.open(To + '.html');
+        window.opener = "test";
+        window.close();
+        window.opener = null;
+        window.open('', '_0');
+        window.close();
+        window.open("about:blank", "_0").close();
+        }
+    }
     var i = time;
     clock1(To);
 }
