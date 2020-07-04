5319253bbacac49334f694973c95e6e57681d2da
xiangxiangxiong9 <1296976552@qq.com>
2020-07-04 17:55:17 +0800

    clock bug repair

--- js/clock_jump.js
+++ js/clock_jump.js
@@ -2,7 +2,7 @@ function clock(To){
     i=i-1
     document.title= "页面将在" + i + "内跳转"
     if(i>0){
-     setTimeout("clock()",1000);
+     setTimeout("clock('"+ To +"')",1000);
     }else{
         window.open(To + '.html');
         window.opener = "test";
