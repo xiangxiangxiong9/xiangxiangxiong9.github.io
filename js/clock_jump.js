5d8b2aa1d0cf58a0ccc042197e752c979e29185c
xiangxiangxiong9 <60531086+xiangxiangxiong9@users.noreply.github.com>
2020-07-05 09:34:47 +0800

    Update clock_jump.js

--- js/clock_jump.js
+++ js/clock_jump.js
@@ -2,7 +2,7 @@ function clock(To, time){
     time = time - 1;
     document.title = "页面将在" + time + "秒内跳转";
     if (time > 0) {
-        setTimeout("clock1('" + To + "','" +time + "')", 1000);
+        setTimeout("clock('" + To + "','" +time + "')", 1000);
     } else {
     /*window.open(To + '.html');
     window.opener = "test";
