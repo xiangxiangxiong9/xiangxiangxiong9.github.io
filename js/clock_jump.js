9dc8af7308d9322ef8e2a52135fb3fe6214d2526
xiangxiangxiong9 <1296976552@qq.com>
2020-07-05 09:46:02 +0800

    禁止返回の测试

--- js/clock_jump.js
+++ js/clock_jump.js
@@ -1,36 +1,16 @@
-function clock1(To){
-    i = i - 1;
-    document.title = "页面将在" + i + "内跳转";
-    if (i > 0) {
-        setTimeout("clock1('" + To + "')", 1000);
+function clock(To, time){
+    time = time - 1;
+    document.title = "页面将在" + time + "秒内跳转";
+    if (time > 0) {
+        setTimeout("clock('" + To + "','" +time + "')", 1000);
     } else {
-    window.open(To + '.html');
+    /*window.open(To + '.html');
     window.opener = "test";
     window.close();
     window.opener = null;
     window.open('', '_0');
     window.close();
-    window.open("about:blank", "_0").close();
+    window.open("about:blank", "_0").close();*/
+    window.location.href=To
     }
-}
-function clock(To, time){
-    for(var i = 0; i < time; i++){
-        (function(i){
-            setTimeout(function(){
-                console.log(i);
-                if(i = time){
-                    i = 0;
-                    window.open(To + '.html');
-                    window.opener = "test";
-                    window.close();
-                    window.opener = null;
-                    window.open('', '_0');
-                    window.close();
-                    window.open("about:blank", "_0").close();
-                }
-            },i * 1000);
-        })(i,To,time)
-        console.log('now' + i);
-    }
-    console.log(i);
 }
