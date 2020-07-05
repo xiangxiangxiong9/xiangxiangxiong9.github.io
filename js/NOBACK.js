a1a61fa8fafc4b5ea2b168ce7015a39886fc5c82
xiangxiangxiong9 <1296976552@qq.com>
2020-07-05 10:09:25 +0800

    ~~~~~~

--- js/NOBACK.js
+++ js/NOBACK.js
@@ -1,4 +1,12 @@
-history.pushState(null, null, document.URL);
-window.addEventListener('popstate', function(e) {
-    history.pushState(null, null, document.URL);
-})
+$(function() {
+	　　if (window.history && window.history.pushState) {
+	　　$(window).on('popstate', function () {
+	　　window.history.pushState('forward', null, '#');
+	　　window.history.forward(1);
+	　　});
+	　　}
+	　　window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+	　　window.history.forward(1);
+	　　})
+	
+	 
