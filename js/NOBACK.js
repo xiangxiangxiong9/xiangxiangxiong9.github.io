733efe864714aa4e080de079ba07574050938d3d
xiangxiangxiong9 <1296976552@qq.com>
2020-07-05 10:04:40 +0800

    ~~~~~

--- js/NOBACK.js
+++ js/NOBACK.js
@@ -1,4 +1,4 @@
-history.pushState(null,null,document.URL);
-window.addEventListener('popstate',function(){
-	history.pushState(null,null,document.URL);
-});
+history.pushState(null, null, document.URL);
+window.addEventListener('popstate', function(e) {
+    history.pushState(null, null, document.URL);
+})
