88d644043b609d879e1f37b492daa1bb0800479c
xiangxiangxiong9 <60531086+xiangxiangxiong9@users.noreply.github.com>
2020-07-05 09:13:04 +0800

    Update clock_jump.js

--- js/clock_jump.js
+++ js/clock_jump.js
@@ -1,4 +1,4 @@
-function clock1(To){
+function clock1(To,i){
     i = i - 1;
     document.title = "页面将在" + i + "内跳转";
     if (i > 0) {
@@ -13,33 +13,6 @@ function clock1(To){
     window.open("about:blank", "_0").close();
     }
 }
-/*function clock(To, time){
-    for(var i = 0; i < time; i++){
-        (function(j,To,time){
-            setTimeout(function(){
-                console.log(i);
-                if(j = time){
-                    j = 0;
-                    window.open(To + '.html');
-                    window.opener = "test";
-                    window.close();
-                    window.opener = null;
-                    window.open('', '_0');
-                    window.close();
-                    window.open("about:blank", "_0").close();
-                }
-            },j * 1000);
-        })(i,To,time)
-        console.log('now' + i);
-    }
-    console.log(i);
-}*/
-const sleep = (timeout= 1000)=>new Promise((resolve, reject)=>{
-    setTimeout(resolve, timeout);
-});
-let clock = async(timeout)=> {
-    for(let i = 0;i<timeout;i++){
-        await sleep(1000);
-        console.log(i+1);
-    }
+function clock(To, time){
+    function clock1(To,time)
 }
