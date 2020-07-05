2760262319d5c1d654d93b7a9080f12ee175656b
xiangxiangxiong9 <1296976552@qq.com>
2020-07-05 10:37:02 +0800

    ~~~~~~~~~~~~~~~~~~~~~~~

--- js/NOBACK.js
+++ js/NOBACK.js
@@ -19,26 +19,11 @@ history.pushState('forward', null, document.URL);
 history.pushState('forward', null, document.URL);
 history.pushState('forward', null, document.URL);
 history.pushState('forward', null, document.URL);
+window.addEventListener('pageshow', function(e) {
+    if (e.persisted) {
+        location.reload();
+    }
+})
 window.addEventListener('popstate', function(e) {
-	history.pushState(null, null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
-	history.pushState('forward', null, document.URL);
+    history.pushState(null, null, document.URL);
 })
