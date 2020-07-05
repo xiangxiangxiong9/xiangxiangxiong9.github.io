b9e3f55ae3e5798c3143bdbc3c3971eaebebe754
xiangxiangxiong9 <1296976552@qq.com>
2020-07-05 10:21:05 +0800

    制造大量的返回...

--- js/NOBACK.js
+++ js/NOBACK.js
@@ -1,12 +1,84 @@
-$(function() {
-	　　if (window.history && window.history.pushState) {
-	　　$(window).on('popstate', function () {
-	　　window.history.pushState('forward', null, '#');
-	　　window.history.forward(1);
-	　　});
-	　　}
-	　　window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
-	　　window.history.forward(1);
-	　　})
-	
-	 
+history.pushState(null, null, document.URL);
+window.addEventListener('popstate', function(e) {
+	history.pushState(null, null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+	window.history.pushState('forward', null, document.URL);
+})
