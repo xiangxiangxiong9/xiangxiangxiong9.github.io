540fb157ecb52cb8870c12f5c124a051bd05108f
xiangxiangxiong9 <1296976552@qq.com>
2020-07-07 21:40:12 +0800

    随机选项应用

---
+++ js/touched.js
@@ -0,0 +1,33 @@
+function touch(Level,ID,Did){
+    if (Did = 0){
+        SaveCookie('Level'+Level,ID,'30');
+        clock('./Level1-A',1);
+    }else{
+        alert('你似乎已经答过这道题了！');
+        }  
+}
+function check(Level){
+    var Did = 0;
+    var cookie_val = ReadCookie("Level"+Level);
+    if (cookie_val == 'A'){
+        document.getElementById("text").innerHTML = "你似乎已经答过这道题了，即将跳转...<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+        clock('/Level2/Level2','3');
+        Did = 1;
+    }
+    if (cookie_val == 'B'){
+        document.getElementById("text").innerHTML = "你似乎已经答过这道题了，即将跳转...<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+        clock('/Level2/Level2','3');
+        Did = 1;
+    }
+    if (cookie_val == 'C'){
+        document.getElementById("text").innerHTML = "你似乎已经答过这道题了，即将跳转...<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+        clock('/Level2/Level2','3');
+        Did = 1;
+    }
+    if (cookie_val == 'D'){
+        document.getElementById("text").innerHTML = "你似乎已经答过这道题了，即将跳转...<br><br><br><br<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+        clock('/Level2/Level2','3');
+        Did = 1;
+    }
+    return Did
+}
