d6a2de6124635ae1b1f88325be8d416bc642b827
xiangxiangxiong9 <1296976552@qq.com>
2020-07-07 21:07:41 +0800

    Level1答题后页面同步&random随机数

--- js/random.js
+++ js/random.js
@@ -1,10 +1,10 @@
-function ramdon(start, end) {
+ function random(start, end) {
     var length = end - start;
     var myorder = new Array();
     var index = 0;
     while (index < length+1) {
     var flag = true;
-    var num = parseInt(start + Math.random() * end);
+    var num = parseInt(start + Math.random() * (end-start+1));
     for (var i in myorder) {
         if (myorder[i] == num) {
                 flag = false;
