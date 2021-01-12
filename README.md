bac58330b8096b56fbf7c68159669caeb93eaa6b
xiangxiangxiong9 <1296976552@qq.com>
2021-01-12 16:49:44 +0800

    页面已迁移...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 README.md 是什么？qwq
+# 此网站已迁移至 jp-tyo-dvm.sakurafrp.com:34039
--- js/check_phone.js
+++
@@ -1,3 +0,0 @@
-function isMoblie() {
-    return !!navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
- }
